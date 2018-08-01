3f8c2e99a2758477c7e35282e64339d22a398f9c
Daniele Ricci <daniele@casaricci.it>
2018-08-01 17:20:45 +0200

    Dummy connect and disconnect commands
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

---
+++ src/main/java/org/kontalk/konbot/shell/commands/DisconnectCommand.java
@@ -0,0 +1,48 @@
+/*
+ * Konbot
+ * Copyright (C) 2018 Kontalk Devteam <devteam@kontalk.org>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kontalk.konbot.shell.commands;
+
+import org.kontalk.konbot.shell.HelpableCommand;
+
+import java.util.Map;
+
+
+public class DisconnectCommand extends AbstractCommand implements HelpableCommand {
+
+    @Override
+    public String name() {
+        return "disconnect";
+    }
+
+    @Override
+    public String description() {
+        return "Disconnect from server";
+    }
+
+    @Override
+    public void run(String[] args, Map<String, Object> session) {
+        // TODO connect
+        println("Not implemented.");
+    }
+
+    @Override
+    public void help() {
+        println("Usage: "+name());
+    }
+}
